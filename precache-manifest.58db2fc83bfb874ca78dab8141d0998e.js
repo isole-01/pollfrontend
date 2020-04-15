ab6ecc8890a02189d8eafb7f253f0cade9359f07
isole-01 <captain.atr007@gmail.com>
2020-04-16 01:38:50 +0430

    Updates

---
+++ precache-manifest.58db2fc83bfb874ca78dab8141d0998e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51fd424b607449c04be9d9147d06eb1",
+    "url": "/pollfrontend/index.html"
+  },
+  {
+    "revision": "e7be0dd61ea906790ac2",
+    "url": "/pollfrontend/static/css/main.81b343f6.chunk.css"
+  },
+  {
+    "revision": "4273ef819d7a1334c928",
+    "url": "/pollfrontend/static/js/2.8373c21d.chunk.js"
+  },
+  {
+    "revision": "ef76a770f9dc5e00cf53d94b664c7e51",
+    "url": "/pollfrontend/static/js/2.8373c21d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7be0dd61ea906790ac2",
+    "url": "/pollfrontend/static/js/main.f0b6e0eb.chunk.js"
+  },
+  {
+    "revision": "1d6858039bbceaace3ff",
+    "url": "/pollfrontend/static/js/runtime-main.bea70e8b.js"
+  }
+]);
